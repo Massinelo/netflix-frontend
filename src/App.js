bdadd5dabeab4fd37d5d550c74f6047a8a58dc5c
Massinelo <massi.ait@gmail.com>
2021-01-21 20:59:15 +0100

    fix style v2

--- src/App.js
+++ src/App.js
@@ -8,29 +8,41 @@ import Footer from './components/Footer';
 function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(
-        'https://raw.githubusercontent.com/Massinelo/netflix-frontend/master/src/assets/data.json'
-      );
-      setData(response.data);
-      setIsLoading(false);
+      try {
+        const response = await axios.get(
+          'https://raw.githubusercontent.com/Massinelo/netflix-frontend/master/src/assets/data.json'
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log(error.message);
+        setError('an error has occurred');
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchData();
   }, []);
-  return isLoading ? (
-    <p>En cours de chargement ...</p>
-  ) : (
-    <>
-      <div className="container">
-        <Header />
-        {data.map((item, index) => {
-          return <Section key={index} dataSection={item} />;
-        })}
-        <Footer />
-      </div>
-    </>
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div className="container">
+      <Header />
+      {data.map((item, index) => (
+        <Section key={index} dataSection={item} />
+      ))}
+      <Footer />
+    </div>
   );
 }
 
